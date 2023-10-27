a20f4e264c7e8913f57a71a038df49c252619d6d
Andrieliliebl <andrieliliebl@gmail.com>
2023-10-27 15:07:31 -0300

    Atualização dia 27/10: finalização do design

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
-const navEl = document.querySelector('nav');
+const navEl = document.querySelector('.my-nav');
 
 window.addEventListener('scroll', () => {
-    if (window.scrollY > 50) {
+    if (window.scrollY > 56) {
         navEl.classList.add('navbar-scrolled');
-    } else if (window.scrollY <= 50) {
+    } else if (window.scrollY <= 56) {
         navEl.classList.remove('navbar-scrolled');
     }
 });
+
+window.onload = function () {
+
+    let aboutMeLink = document.getElementById("about-me-link");
+    aboutMeLink.addEventListener('click', () => {
+        let mobileNavBar = document.getElementById("offcanvasNavbar");
+        mobileNavBar.classList.remove("show");
+    });
+
+    let projectsLink = document.getElementById("projects-link");
+    projectsLink.addEventListener('click', () => {
+        let mobileNavBar = document.getElementById("offcanvasNavbar");
+        mobileNavBar.classList.remove("show");
+    });
+}
+
+let date = new Date ();
+let year = date.getFullYear();
+document.getElementById('currentYear').innerHTML = year;
